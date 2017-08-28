6f20dc803d938d3792126d2e86855331066531c8
Andrew Pierce <apierce1992@gmail.com>
2017-08-28 12:25:21 -0400

    basic react starter template

---
+++ app/Components/Main.js
@@ -0,0 +1,16 @@
+// Include React
+var React = require("react");
+
+// This is the main component
+var Main = React.createClass({
+
+  // Here we describe this component's render method
+  render: function() {
+    return (
+      <h1> HELLOOOO </h1>
+    );
+  }
+});
+
+// Export the component back for use in other files
+module.exports = Main;
